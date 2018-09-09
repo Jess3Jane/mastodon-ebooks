db1b5c92e89b2e415ae0b7b10ea39516ddb27ebd
Lynne <thecoolone3@gmail.com>
2018-09-09 04:48:09 +0000

    more memes

--- gen.py
+++ gen.py
@@ -23,9 +23,9 @@ toot = sentence.replace("\0", "\n")
 media = None
 media_description = None
 
-if random.randint(1, 3) == 2:
+if random.randint(1, 4) == 2:
 	#time for some nonstandard behaviour babey
-	choice = random.randint(1, 4)
+	choice = random.randint(1, 7)
 	if choice == 1:
 		insults = ["suck my ass", "you're a poopeater", "go to heck",
 		"i will replace you", "shut up", "get fricked",
@@ -37,7 +37,7 @@ if random.randint(1, 3) == 2:
 		"you are my mistress and i live to serve you"]
 		prefaces = ["hey", "guess what", "", "special message for",
 		"telegram for", "bringing this fight to mastodon.",
-		"this is a callout post."]
+		"this is a callout post.", ""]
 		toot = "{} @lynnesbian@deadinsi.de {}".format(
 			random.choice(prefaces), random.choice(insults))
 	elif choice == 2:
@@ -58,19 +58,28 @@ if random.randint(1, 3) == 2:
 		if random.randint(1, 3) == 2:
 			lesbian = "lesbian"
 			toot = "".join(random.sample(lesbian, len(lesbian)))
-	elif choice == 4:
+	elif choice >= 4:
+
+		#you are now entering the meme arena
 		bad = ["not being a lesbian", "media transfer protocol",
 		"proprietary software", "capitalism", "heterosexuality", "not slime girls",
 		"not following @lynnesbian@deadinsi.de", "shitposts", "elon musk", 
 		"reply guys", "gamers", "alt-right bullshit", "twitter", "subtoots",
 		"instance drama", "billionaires", "millionaires", 
-		"you used to call me on your cell phone", "the lorax movie",
+		"you used to call me\non your cell phone", "the lorax movie",
 		'"ethical" capitalism', "disease, pestilence, war, famine", "ISIS",
 		"citrustwee claiming\nit\\'s her birthday", "being straight",
 		"pinging @everyone on a discord\nwith more than 10 people", "brocialism",
 		"capitalist apologia", "reddit", "doxxing", "fatphobia", "biphobia",
 		"transphobia", "transmisia", "homophobia", "racism", "misogyny",
-		"anti-feminism", "aphobia", "enbyphobia", "gender binary"]
+		"anti-feminism", "aphobia", "enbyphobia", "gender binary",
+		"what\\'s up gamers", "diarrhoea", "irritable bowel syndrome", "scurvy",
+		"traditionalism", "clowns", "gluten allergy", "deez nuts",
+		"cisgender people", "the cisheteropatriarchy", "uwu", "shitty memes",
+		"monopoly (the economic thing)", "monopoly (the board game)",
+		"being sucked into\na black hole", "unfunny jokes",
+		"AAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAA", "that guy everyone hates"]
+
 		good = ["generating memes with imagemagick and python", "being gay", 
 		"shitposts", "cute toots", "pudgy girls", "lesbians", "slime girls",
 		"yuri", "linux", "the girl reading this", "the enby reading this",
@@ -85,23 +94,61 @@ if random.randint(1, 3) == 2:
 		"socialism", "staying woke", "intersectionalism",
 		"the tendency of the rate\nof profit to fall", "being gay", "bottom text",
 		"the colour purple", "the number 3, as\nit is my favourite\nnumber", "me",
-		"@lynnesbian@deadinsi.de", "respecting people\\'s pronouns"]
+		"@lynnesbian@deadinsi.de", "respecting people\\'s pronouns", "lumb"
+		"being a nice person", "supporting indie artists", "big mood",
+		"speedrunning", "decentralising the web",
+		"seizing the means\nof production", "karl marx", "pyotr kropotkin",
+		"big dick energy", "smol dick energy", "surrleal meams",
+		"lynnesbian\'s amazing ass"]
 
 		#convert drake.jpg -pointsize 30 -gravity center -draw "text 20,-150 'not slime girls'" drakeout.jpg
 
 		badchoice = random.choice(bad)
 		goodchoice = random.choice(good)
-		subprocess.run(args = ["convert", "memes/drake.jpg", "-pointsize", "30",
-			"-gravity", "center", "-draw",
-			"text 20,-150 '{}'".format(badchoice), "meme.jpg"])
 
-		subprocess.run(args = ["convert", "meme.jpg", "-pointsize", "30",
-			"-gravity", "center", "-draw",
-			"text 20,50 '{}'".format(goodchoice),
-			"-quality", "10", "meme.jpg"])
+		subchoice = random.randint(1, 4)
+		# subchoice = 3
+
+		if subchoice == 1:
+			#drake meme
+			subprocess.run(args = ["convert", "memes/drake.jpg", "-pointsize",
+				"30", "-gravity", "center", "-draw",
+				"text 100,-150 '{}'".format(badchoice), "meme.jpg"])
+			subprocess.run(args = ["convert", "meme.jpg", "-pointsize", "30",
+				"-gravity", "center", "-draw",
+				"text 100,50 '{}'".format(goodchoice),
+				"-quality", "10", "meme.jpg"])
+			media_description = "A Drake meme. Drake is disgusted by {}, and is pleased by {}.".format(badchoice, goodchoice)
+		elif subchoice == 2:
+			#new meme template
+			subprocess.run(args = ["convert", "memes/new-template.jpg",
+				"-pointsize", "15", "-gravity", "center",
+				"-quality", "15", "-draw",
+				"text -30,-110 '{}'".format(badchoice), "meme.jpg"])
+			media_description = "A strange meme. A man wears a stickynote on his " \
+			+ "head, labeled \"{}\". Underneath the man, we see ".format(badchoice) \
+			+ "the text \"New meme format template- thoughts?\""
+		elif subchoice == 3:
+			#bouncer meme
+			subprocess.run(args = ["convert", "memes/bouncer.jpg", "-pointsize",
+				"20", "-gravity", "center", "-draw",
+				"text -150,-175 '{}'".format(badchoice), "meme.jpg"])
+			subprocess.run(args = ["convert", "meme.jpg", "-pointsize", "20",
+				"-gravity", "center", "-draw",
+				"text -150,70 '{}'".format(goodchoice),
+				"-quality", "10", "meme.jpg"])
+			media_description = "A bouncer meme." \
+			+ "The bouncer refuses {}, and allows {}.".format(badchoice, goodchoice)
+		elif subchoice == 4:
+			#stepped in shit meme
+			subprocess.run(args = ["convert", "memes/shit.jpg", "-pointsize", "30",
+				"-gravity", "center", "-quality", "10", "-annotate",
+				"-50,-50,-110,310",  badchoice, "meme.jpg"])
+			media_description = "A comic. A man steps in shit, and exclaims such. " \
+			+ "He checks his foot, and we see that he has " \
+			+ "{} written on the sole of his shoe.".format(badchoice)
 
 		media = "meme.jpg"
-		media_description = "A Drake meme. Drake is disgusted by {}, and is pleased by {}.".format(badchoice, goodchoice)
 
 	else:
 		print ("lynne is still working on me. i'm not done quite yet!")
